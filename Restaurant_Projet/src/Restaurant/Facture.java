f56b4b9f93826b52eaeffb900e165d3ca09e506d
merveille-kadji-3il <kadjiyom@3il.fr>
2025-05-20 16:08:36 +0200

    Ajout facture

--- Restaurant_Projet/src/Restaurant/Facture.java
+++ Restaurant_Projet/src/Restaurant/Facture.java
@@ -1,5 +1,23 @@
 package Restaurant;
 
 public class Facture {
-
+	private int id;
+	private double monTotal;
+	private String statutPaiement;
+	
+	public Facture(int id, double monTotal, String statutPaiement) {
+		super();
+		this.id = id;
+		this.monTotal = monTotal;
+		this.statutPaiement = statutPaiement;
+	}
+	
+	public void generer() {
+		
+	}
+	
+	public void enregistrerPaiement() {
+		
+	}
+	
 }
