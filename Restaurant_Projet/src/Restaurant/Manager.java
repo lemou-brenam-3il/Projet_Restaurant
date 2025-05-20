fb4c99241cfab0844d8fbf25b6f83d66de2e9cd7
merveille-kadji-3il <kadjiyom@3il.fr>
2025-05-20 16:04:11 +0200

    Ajout des classes

---
+++ Restaurant_Projet/src/Restaurant/Manager.java
@@ -0,0 +1,7 @@
+package Restaurant;
+
+public class Manager extends Personnel{
+	public Manager(int id, String nom, String role) {
+		super(id, nom, role);
+	}
+}
---
+++ Restaurant_Projet/src/Restaurant/Reservation.java
@@ -0,0 +1,30 @@
+package Restaurant;
+
+import java.util.Date;
+
+public class Reservation {
+	private int id;
+	private String clientNom;
+	private Date dateHeure;
+	private int nombrePersonnes;
+	
+	public Reservation(int id, String clientNom, Date dateHeure, int nombrePersonnes) {
+		super();
+		this.id = id;
+		this.clientNom = clientNom;
+		this.dateHeure = dateHeure;
+		this.nombrePersonnes = nombrePersonnes;
+	}
+	
+	public void creer() {
+		
+	}
+	
+	public void modifier() {
+		
+	}
+	
+	public void annuler() {
+		
+	}
+}
